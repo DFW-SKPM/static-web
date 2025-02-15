d431a177e1a3f2cfcf998646af2a07017bef00d7
Sujan Khatiwada <82593248+sujanaryan@users.noreply.github.com>
2025-02-14 19:58:31 -0600

    Update main.js
    Count down update

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ $(window).on('load', function() {
 	/*------------------
 		Counter
 	--------------------*/
-	$(".counter").countdown("2024/10/27", function(event) {
+	$(".counter").countdown("2025/02/15", function(event) {
 		$(this).html(event.strftime("<div class='counter-item'><h4>%D</h4>Days</div>" + "<div class='counter-item'><h4>%H</h4>hours</div>" + "<div class='counter-item'><h4>%M</h4>Mins</div>" + "<div class='counter-item'><h4>%S</h4>secs</div>"));
 	});
 
